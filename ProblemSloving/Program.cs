ecf154f1bee5a2b7cb234aabd49d30eceeb3fc93
DESKTOP-J37CM3Q\Shimea <shimaahmady@gmail.com>
2025-04-14 23:14:20 +0200

    add 2 problems

--- ProblemSloving/Program.cs
+++ ProblemSloving/Program.cs
@@ -30,6 +30,49 @@ void Merge(int[] nums1, int m, int[] nums2, int n)
 }
 
 
+int RemoveElement(int[] nums, int val)
+{
+    List<int> expectednums = new();
+    int k = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != val)
+        {
+            k++;
+            expectednums.Add(nums[i]);
+        }
+
+    }
+
+    for (int i = 0; i < expectednums.Count; i++)
+    {
+        nums[i] = expectednums[i];
+    }
+    return k;
+
+}
+
+int RemoveDuplicates(int[] nums)
+{
+    int dublicateIndex = 0;
+    int originalIndex = 0;
+    int k = 0;
+
+    while (dublicateIndex + 1 <= nums.Length)
+    {
+        if (dublicateIndex + 1 == nums.Length)
+            nums[originalIndex] = nums[dublicateIndex];
+        else if (nums[dublicateIndex] != nums[dublicateIndex + 1])
+        {
+            nums[originalIndex] = nums[dublicateIndex];
+            originalIndex++;
+        }
+        dublicateIndex++;
+    }
+    k = originalIndex + 1;
+    return k;
+}
+
 
 
 
